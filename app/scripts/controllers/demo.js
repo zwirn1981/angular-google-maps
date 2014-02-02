eb35159cd74c1ce3a29b52f03931c208d1d5731c
Nicolas Laplante <nicolas.laplante@gmail.com>
2014-02-02 11:27:23 -0500

    Work on official demo

--- app/scripts/controllers/demo.js
+++ app/scripts/controllers/demo.js
@@ -1,8 +1,100 @@
 'use strict';
 
-angular.module('angularGoogleMapsApp').controller('DemoCtrl', function ($scope, $timeout) {
+angular.module('angularGoogleMapsApp').controller('DemoCtrl', function ($scope, $timeout, $http) {	
 	
-	$scope.tab = 'status';
+	function randomLatitude () {
+		var num = Math.floor(Math.random() * 75) + 1;
+		
+		if ((Math.floor(Math.random() * 10) + 1) % 2 === 0) {
+			return -num;
+		}
+		
+		return num;
+	}
+	
+	function randomLongitude () {
+		var num = Math.floor(Math.random() * 180) + 1;
+		
+		if ((Math.floor(Math.random() * 10) + 1) % 2 === 0) {
+			return -num;
+		}
+		
+		return num;
+	}
+	
+	var lastMarkerId = 0;
+	function generateRandomMarkers (num) {
+		var cnt = $scope.map.markers.length;
+		
+		for (var i = 0; i < num; i++) {
+			$scope.map.markers.push({
+				id: ++lastMarkerId,
+				title: 'Marker ' + lastMarkerId,
+				coords: {
+					latitude: randomLatitude(),
+					longitude: randomLongitude()
+				}			
+			});
+		}
+	};
+	
+	var lastPolylineId = 0;
+	function generateRandomPolyline (num) {
+		var cnt = $scope.map.polylines.length;
+		
+		for (var i = 0; i < num; i++) {
+		
+			var path = [];
+			
+			for (var j = 0; j < 4; j++) {
+				path.push({
+					latitude: randomLatitude(),
+					longitude: randomLongitude()
+				})
+			}
+		
+			$scope.map.polylines.push({
+				id: ++lastPolylineId,
+				title: 'Polyline ' + lastPolylineId,
+				path: path,
+				clickable: true,
+				editable: true,
+				geodesic: true,
+				draggable: true
+			});
+		}
+	}
+	
+	var lastPolygonId = 0;
+	function generateRandomPolygon (num) {
+		var cnt = $scope.map.polygons.length;
+		
+		for (var i = 0; i < num; i++) {
+		
+			var path = [];
+			
+			for (var j = 0; j < 4; j++) {
+				path.push({
+					latitude: randomLatitude(),
+					longitude: randomLongitude()
+				})
+			}
+		
+			$scope.map.polygons.push({
+				id: ++lastPolygonId,
+				title: 'Polyline ' + lastPolygonId,
+				path: path,
+				fill: {
+					color: '#ff0000',
+					opacity: 0.6
+				},
+				clickable: true,
+				editable: true,
+				geodesic: true,
+				draggable: true
+			});
+		}
+	}
 	
 	$scope.map = {
 		center: {
@@ -10,50 +102,41 @@ angular.module('angularGoogleMapsApp').controller('DemoCtrl', function ($scope, 
 			longitude: -73.85	// NYC
 		},
 		zoom: 8,
-		markers: [{
-				id: 0,
-				coords: {
-					latitude: 41,
-					longitude: -75
-				},				
-				title: 'Marker 1'
-			}, {
-				id: 1,
-				coords: {
-					latitude: 40,
-					longitude: -74.5
-				},				
-				title: 'Marker 2'				
-			}],
-		polyline: {
-			path: [{
-				latitude: 41,
-				longitude: -75
-			}, {
-				latitude: 40,
-				longitude: -74.5			
-			}, {
-				latitude: 40.47,
-				longitude: -73.85
-			}, {
-				latitude: 41.2,
-				longitude: -74.2
-			}],
-			clickable: true,
-			editable: true,
-			geodesic: true,
-			draggable: true
-		}
+		markers: [],
+		polylines: [],
+		polygons: []
 	};	
 	
-	$timeout(function () {
-		$scope.map.markers.push({
-			id: 3,
-			coords: {
-				latitude: 40.2,
-				longitude: -74.3
-			},
-			title: 'Marker 3'			
+	$scope.addMarker = function () {
+		generateRandomMarkers(1);
+	};
+	
+	$scope.removeMarker = function (marker) {	
+		var index = _.indexOf($scope.map.markers, marker);
+		
+		if (index !== -1) {
+			$scope.map.markers.splice(index, 1);
+		}
+		
+		if (!$scope.map.markers.length) {
+			$scope.map.zoom = 8;
+		}
+	};
+	
+	$scope.addPolyline = function () {
+		generateRandomPolyline(1);
+	};
+	
+	$scope.removePolyline = function (p) {
+		var index = _.indexOf($scope.map.polylines, p);
+		
+		if (index !== -1) {
+			$scope.map.polylines.splice(index, 1);
+		}
+	};
+	
+	$http.get('https://rawgithub.com/nlaplante/angular-google-maps/website/app/views/demo.html').
+		then(function (response) {
+			$scope.templateHtml = response.data;
 		});
-	}, 4000);
 });
